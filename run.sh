e4a4705cac9d45e304e18385e2b3c2c376c41d79
angredukut <156060884+angredukut@users.noreply.github.com>
2024-04-25 16:48:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"starknet_getBlockByNumber","params":["latest", false],"id":1}' https://rpc.starknet.lava.build/lava-referer-b116b8a5-77d8-44e2-b476-c9f438c44335/)
 
 
 echo "$curl_output" >> Update.tsx
